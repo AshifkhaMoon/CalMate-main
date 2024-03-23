09e34f72813da4ffa432db8b748babf5aa94f499
m3na0 <m3na02@gmail.com>
2024-03-23 17:09:26 +0200

    Functionality of CalculateCalories

--- app/src/main/java/com/example/calmate/CalculateCalories.kt
+++ app/src/main/java/com/example/calmate/CalculateCalories.kt
@@ -2,10 +2,22 @@ package com.example.calmate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class CalculateCalories : AppCompatActivity() {
+
+    private lateinit var ageEditText: EditText
+    private lateinit var heightEditText: EditText
+    private lateinit var weightEditText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculate_calories)
+
+        ageEditText = findViewById(R.id.age_EditText)
+        heightEditText = findViewById(R.id.height_EditText)
+        weightEditText = findViewById(R.id.weight_EditText)
+
+
     }
 }
