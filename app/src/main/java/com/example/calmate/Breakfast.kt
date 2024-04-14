7154ee5dac7e002692e7e7371d663a863b2f260a
m3na0 <m3na02@gmail.com>
2024-04-14 16:49:08 +0300

    Made some changes in Food Menu Implmentation

---
+++ app/src/main/java/com/example/calmate/Breakfast.kt
@@ -0,0 +1,15 @@
+package com.example.calmate
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Breakfast : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_breakfast)
+
+        }
+    }
